e70c6854a6a4673489566ba2a1c8a8c3e3398d33
K0101202 <k0101202@test.htsc.com.cn>
2019-10-18 16:40:53 +0800

    Initial commit

---
+++ python_train.py
@@ -0,0 +1,38 @@
+print("Hello World！")
+# 我是注释
+
+'''
+我也是注释
+'''
+
+"""
+我还是注释~
+"""
+
+print("Hello Python!")
+
+# 缩进表示代码块，且缩进必须相同
+if True:
+    print("True")
+else:
+    print("False")
+
+# 多行语句 使用 \ 连接
+total = "1" + \
+        "2" + \
+        "3"
+print(total)
+
+# 4种数字类型
+# int 整数
+number = 1
+# bool
+number = True
+# float
+number = 1.2
+number = 3E-2
+# complex 复数
+number = 1 + 2j
+
+# 字符串
+# 单 双引号 完全相同
